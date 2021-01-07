6c8075bb2607a1af40691d2b685a0571e8b7c929
Brianhin94 <brianhin64@gmail.com>
2021-01-07 10:57:31 -0700

    counter for assesment 1
    Counter for assesment 1

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let output = document.getElementById('output');
+    let plusButton = document.getElementById('plusBtn');
+    let input = document.getElementById('input');
+    let minusButton = document.getElementById('minusBtn');
+    let number = 0;  
+    plusButton.addEventListener("click", function(e) {
+        number = +number + +input.value;
+        output.innerText = number;
+        if (number < 0) {
+            output.style.color = "red"
+        } else if (number > 0) {
+            output.style.color = "black"
+        }
+    });
+    minusButton.addEventListener("click", function(e) {
+        number = +number - +input.value;
+        output.innerText = number;
+        if (number < 0) {
+            output.style.color = "red"
+        } else if (number > 0) {
+            output.style.color = "black"
+        }
+    }) 
+    })
